4266c864bfc183c27dc6b61248c799b5598fe8c9
Kyleth22 <vegito1200@hotmail.com>
2026-02-27 01:11:18 -0600

    Change base path in Vite config to root

--- frontend-react/vite.config.js
+++ frontend-react/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/LEPARFUM_PF/', // <--- AGREGA ESTA LÍNEA EXACTAMENTE ASÍ
+  base: '/', // <--- AGREGA ESTA LÍNEA EXACTAMENTE ASÍ
 })
